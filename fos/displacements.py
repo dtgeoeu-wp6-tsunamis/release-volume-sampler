2a31eafc767364fef2f04adcce0e1f5bd22ec7fe
Erlend Briseid Storrøsten <erlend.briseid.storrosten@ngi.no>
2024-09-19 13:28:26 +0000

    Calculate yield acceleration quantiles.

--- fos/displacements.py
+++ fos/displacements.py
@@ -35,36 +35,55 @@ def write_tif(fname, data, profile):
         dst.write(data, 1)
 
 
-def get_fos(slope_data, params):
+def infinite_slope_analysis(slope, friction_angle, cohesion, thickness, density, density_of_water, gravity = 9.81, excess_pore_pressure = 0., yield_angle = None):
     """
-    Calculation of FOS with no ground movement (s = 0).
     
-    params = {
-        "friction_angle": 24.3,         # [degrees]     normal distribution
-        "cohesion": 20.,                # [kPa]         lognormal distribution
-        "thickness": 4,                 # depth to slide surface measured along slope normal [m].
-        "density": 2000,                # density of slide [kg/m3]
-        "density_of_water": 1020,       # density of water [kg/m3]
-        "gravity": 9.81,                # [m/s2]
-        "excess_pore_pressure": 0.      # [kPa]
-    }
+    Returns Factor of Safety and Yield Acelleration associated with an infinite slope analysis.
+    Yield acceleration is expressed in multiples of gravity. Cyrrently drained/undrained conditions are not considered.
+    
+    Parameters: 
+    
+    slope: number or ndarray
+        Slope angle in degrees.
+    friction_angle:
+        friction angle in degrees.
+    cohesion: 
+        Cohesion [kPa]
+    thickness:
+        depth to slide surface measured slope normal [m].
+    density:
+        density of slide [kg/m3]
+    density_of_water:
+        density of water [kg/m3]
+    gravity:
+        acceleration of gravitation [m/s2] (default is 9.81)
+    excess_pore_pressure:
+        Excess pore pressure [kPa] (default is 0)
+    yield_angle:
+        Direction of shaking given in degrees with horizontal axis. If None, shaking is assumed to be parallell with slope.
+    TODO: Verify correction factor for yield acelleration!
     """
     logger.info("Calculating Factor Of Safety.")
     
-    gamma = (params["density"] - params["density_of_water"])/params["density"]
-    c = params["cohesion"]*1000. #in Pa
-    u = params["excess_pore_pressure"]*1000. #in Pa
-    mu = np.tan(np.radians(params["friction_angle"]))
-    g = params["gravity"]
-    rho = params["density"]
-    H = params["thickness"]
-    alpha = np.radians(slope_data)
+    gamma = (density - density_of_water)/density
+    c = cohesion*1000. #in Pa
+    u = excess_pore_pressure*1000. #in Pa
+    mu = np.tan(np.radians(friction_angle))
+    g = gravity
+    rho = density
+    H = thickness
+    alpha = np.radians(slope)
     
-    return (c-u*mu)/(rho*H*gamma*g*np.sin(alpha)) + mu/np.tan(alpha)
+    fos = (c-u*mu)/(rho*H*gamma*g*np.sin(alpha)) + mu/np.tan(alpha)
+    ky = gamma*(np.sin(alpha)*(fos-1.))
+    
+    if yield_angle is not None:
+        psi = np.radians(yield_angle)
+        ky = ky/(np.cos(psi-alpha) - np.sin(psi-alpha)*mu) # Correction factor
+    return fos, ky
 
 
 def main():
-    #logger.addHandler(logging.StreamHandler())
     working_dir = "/home/ebr/projects/release-volume-sampler/generated/messina_001_20240909_130842"
     
     # TODO: Create subdirectory fos
@@ -72,7 +91,12 @@ def main():
     filenames = {
         "slope": "slope.tif"
     }
+    # Miller and Rice (1983) 7 CDF points of normal distribution.
+    cdf_7 = [0.019106, 0.115498, 0.285336, 0.50000, 0.714664, 0.884502, 0.980894] 
+    wt_7 = np.array([0.054866, 0.135893, 0.198097, 0.222288, 0.198097, 0.135893, 0.054866], dtype=np.single)
    
+    quantiles = [0.3, 0.5, 0.8]
+    
     # Parameters defined as discrete distributions or constants.
     # May supply functional relations. Order according to dependencies.
     distributions = {
@@ -89,23 +113,32 @@ def main():
     }
 
     # Create event tree.
-    root_node = Node(weight=1, distributions=distributions,params=params, parent=None)
+    root_node = Node(weight=1, distributions=distributions, params=params, parent=None)
     
-    # Traverse leaf nodes and calculate fos
+    # Traverse leaf nodes and create linear interpolations for slope variation.
     slope_data, slope_profile = read_tif(os.path.join(working_dir, filenames["slope"]))
     
+    # Create slope range for interpolation
+    slopes = np.linspace(np.nanmin(slope_data), np.nanmax(slope_data), num=100)
+    kys, weights, foss = [], [], []
     sum_of_weights = 0.
+
     for count, node in enumerate(root_node.leaf_nodes):
         logger.info(f"Leaf node:{count} \n Weight:{node.weight} \n Params:{node.params} \n")
-        fos = get_fos(slope_data, node.params)
-        # write_tif(fname = os.path.join(working_dir,"fos", f"fos_{count}.tif"), data = fos, profile = slope_profile)
-        
-        # TODO aggregate (approximate) cumulative distribution
-        
+        fos, ky = infinite_slope_analysis(slopes, **node.params)
+        kys.append(ky)
+        weights.append(node.weight)
         sum_of_weights += node.weight
     
+    # Compute quantiles
+    ky_quantiles = np.quantile(np.stack(kys), quantiles, axis=0, weights=np.array(weights), method='inverted_cdf')
     logger.info(f"Sum of weights: {sum_of_weights}")
     
+    # Interpolate quantiles and write to file
+    for i,quantile in enumerate(quantiles):
+        ky_quantiles_raster = np.interp(slope_data, slopes, ky_quantiles[i,:])
+        write_tif(fname = os.path.join(working_dir,"fos", f"ky_{quantile}.tif"), data = ky_quantiles_raster, profile = slope_profile)
+
 
 class Node():
     leaf_nodes = []
